f8c9579b3e131426354d423a4f1048443f439ebf
ktplkr-nihon <ashuengi12@gmail.com>
2021-07-08 15:28:36 +0530

    adding stuff

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../assets/styles/contactus.css';
 const Contactus = () => {
 	return(
-         <div>
-           Contactus
-         </div>
+         <section>
+           <div>
+             <div className="contact-header">
+               <p className="header-txt">CONTACT US</p>
+             </div>
+           </div>
+         </section>
 		);
 }
 export default Contactus;
